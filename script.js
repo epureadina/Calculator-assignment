609f4d7afbbce5c6bfef96e97e9d9d5fa6698580
Adina Epure <predescu.adina@yahoo.com>
2022-10-22 23:21:22 +0300

    Updated commit 2

--- script.js
+++ script.js
@@ -3,31 +3,85 @@ const numberButton = document.querySelectorAll(".number-btn");
 const decimalButton = document.querySelector(".decimal-btn");
 const operatorButton = document.querySelectorAll(".operator");
 const clearButton = document.querySelector(".clear-btn");
+const deleteButton = document.querySelector(".delete-btn");
 const equalButton = document.querySelector(".equal-btn");
+const signButton = document.querySelector(".sign-btn");
 const currentOperand = document.querySelector(".current-operand");
 const previousOperand = document.querySelector(".previous-operand");
 
 //Declaring variables
+currentOperand.textContent = "";
+previousOperand.textContent = "";
 let storedNumber = "";
 let clickedOperator = "";
 let firstNumber = "";
 let result = "";
-
 //Functions
 function add(x, y) {
-  return x + y;
+  return (x + y).toFixed(2);
 }
 function substract(x, y) {
-  return x - y;
+  return (x - y).toFixed(2);
 }
 function multiply(x, y) {
-  return x * y;
+  return (x * y).toFixed(2);
 }
 function divide(x, y) {
-  return y === 0 ? "Can't divide by 0!" : x / y;
+  return y === 0 ? "Can't divide by 0!" : (x / y).toFixed(2);
 }
 function percentage(x) {
   return x / 100;
+}
+function factorial(x) {
+  let y;
+  if (x === 0) {
+    y = 1;
+  } else if (x < 1) {
+    x = "undefined";
+  } else {
+    y = 1;
+    for (let i = x; i > 0; i--) {
+      y *= i;
+    }
+  }
+  return y.toFixed(2);
+}
+function exponent(x, y) {
+  /* let z = 1;
+  if (y === 0) {
+    z = 1;
+  } else if (y < 0) {
+    for (let i = y; i < 0; i++) {
+      z = z * (1 / x);
+    }
+  } else {
+    for (let i = y; i > 0; i--) {
+      z = z * x;
+    }
+  }
+  return z;*/
+  return Math.pow(x, y).toFixed(2);
+}
+function naturalLogaritm(x, y) {
+  return Math.log(y).toFixed(2);
+}
+function cosinus(x, y) {
+  return Math.cos(y).toFixed(2);
+}
+function sinus(x, y) {
+  return Math.sin(y).toFixed(2);
+}
+function tangent(x, y) {
+  return Math.tan(y).toFixed(2);
+}
+function radians(x) {
+  return (x * (Math.PI / 180)).toFixed(2);
+}
+function radical(x, y) {
+  return Math.sqrt(y);
+}
+function Pi(x) {
+  return x * Math.PI;
 }
 function operate(x, y, operator) {
   switch (operator) {
@@ -41,6 +95,24 @@ function operate(x, y, operator) {
       return divide(x, y);
     case "%":
       return percentage(x);
+    case "!":
+      return factorial(x);
+    case "^":
+      return exponent(x, y);
+    case "ln":
+      return naturalLogaritm(x, y);
+    case "cos":
+      return cosinus(x, y);
+    case "sin":
+      return sinus(x, y);
+    case "tan":
+      return tangent(x, y);
+    case "rad":
+      return radians(x);
+    case "&#8730":
+      return radical(x, y);
+    case "pi":
+      return Pi(x);
   }
 }
 function displayResult() {
@@ -53,7 +125,6 @@ function displayResult() {
   previousOperand.textContent =
     firstNumber + " " + clickedOperator + " " + storedNumber;
   storedNumber = result;
-  console.log("FirstNumber" + firstNumber + "Stored" + storedNumber);
   firstNumber = "";
   clickedOperator = "";
   result = "";
@@ -88,12 +159,28 @@ equalButton.addEventListener("click", function () {
 clearButton.addEventListener("click", function () {
   currentOperand.textContent = "";
   previousOperand.textContent = "";
+  firstNumber = "";
   storedNumber = "";
   operatorButton.textContent = "";
 });
 decimalButton.addEventListener("click", function () {
   if (!currentOperand.textContent.includes(".")) {
     storedNumber += ".";
+    currentOperand.textContent = storedNumber;
+  }
+});
+deleteButton.addEventListener("click", function () {
+  arr1 = storedNumber.split("");
+  arr1.pop();
+  storedNumber = arr1.join("");
+  currentOperand.textContent = storedNumber;
+});
+signButton.addEventListener("click", function () {
+  if (storedNumber.charAt(0) === "-") {
+    storedNumber = storedNumber.slice(1);
+    currentOperand.textContent = storedNumber;
+  } else {
+    storedNumber = "-" + storedNumber;
     currentOperand.textContent = storedNumber;
   }
 });
